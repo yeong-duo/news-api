9909153015f123d44a19876224529c1b4fd80e5b
duyoung <aveune@naver.com>
2024-08-06 19:50:22 +0900

    first commit

---
+++ src/main/kotlin/com/yeong_duo/news_api/NewsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.yeong_duo.news_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NewsApiApplication>(*args)
+}
