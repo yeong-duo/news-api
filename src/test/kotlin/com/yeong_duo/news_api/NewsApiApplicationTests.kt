0747cdf00f14f2da3f952325c8d9ecc85457b7f0
duyoung <aveune@naver.com>
2024-08-06 19:50:22 +0900

    first commit

---
+++ src/test/kotlin/com/yeong_duo/news_api/NewsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yeong_duo.news_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
